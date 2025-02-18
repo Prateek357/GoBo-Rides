640139fb04664aa06c506843bc59941a9b4d7f09
Prateek357 <129489993+Prateek357@users.noreply.github.com>
2025-02-18 17:38:06 +0530

    updated integration

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { UserDataContext } from '../context/UserContext'
 const UserSignup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -7,20 +9,32 @@ const UserSignup = () => {
   const [lastname, setLastname] = useState('');
   const [userData, setUserData] = useState({});
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate();
+  const {user,setUser}=React.useContext(UserDataContext);
+
+  const submitHandler = async(e) => {
     e.preventDefault();
     
-    setUserData({ fullname:{firstname:firstname, lastname:lastname}, email:email, password:password });
+    const newUser={ fullname:{firstname:firstname, lastname:lastname}, email:email, password:password };
+    const response=axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser);
+
+    if(response.status===200){
+      const data=response.data;
+      setUser(data.user);
+
+      navigate('/home');
+    }
+
     setEmail('');
     setPassword('');
     setFirstname('');
     setLastname('');
-
-  }
+  
+}
   return (
         <div className='h-screen p-7 flex flex-col justify-between'>
             <div>
-            <img className='mt-9 ml-1 w-16 mb-14' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <img className='mt-9 ml-1 w-16 mb-7' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
             <form onSubmit={(e) => submitHandler(e)}>
       <h3 className='text-lg font-medium mb-2'>What's your name</h3>  
       <div className='flex gap-4 mb-6'> 
@@ -35,7 +49,7 @@ const UserSignup = () => {
         <h3 className='text-lg font-medium mb-2'>Enter password</h3>
 
         <input required value={password} onChange={e => setPassword(e.target.value)} className='bg-[#eeeeee] mb-6 rounded px-4 py-2 w-full text-lg placeholder:text-base' type="password" placeholder='password'/>
-        <button className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'>Login</button>
+        <button className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'>Create Account</button>
       </form>
       <p className='text-center'>Already have an account? <Link to='/login' className='text-blue-600'>Login here</Link></p>
       </div>
